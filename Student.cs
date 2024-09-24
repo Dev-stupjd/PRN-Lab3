d6f90e6c0b29f9f7e66ff4b08336451aa9da9003
Dev-stupjd <khoindqde180179@fpt.edu.vn>
2024-09-24 09:43:55 +0700

    first commit

---
+++ Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3_Collection___Generic
+{
+    public class Student : IComparable<Student>
+    {
+        public int ID { get; set; }
+        public int Age { get; set; }
+        public string FullName { get; set; }
+
+        public int CompareTo(Student other)
+        {
+            return this.Age.CompareTo(other.Age);
+        }
+
+        public override string ToString() => $"ID: {ID}, Age: {Age}, FullName: {FullName}";
+    }
+}
